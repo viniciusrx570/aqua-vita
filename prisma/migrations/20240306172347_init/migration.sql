267c56242e752fa015fbd8329d70d36a12549917
Vinícius <viniciusrx570@gmail.com>
2024-03-07 09:02:19 -0300

    feat: conectado com banco de dadps

--- prisma/migrations/20240306172347_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Clientes" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "nome" TEXT NOT NULL,
-    "cpf" TEXT NOT NULL,
-    "data_nascimento" TEXT NOT NULL,
-    "sexo" TEXT NOT NULL
-);
