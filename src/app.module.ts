4b3070a0da3aa9729eb33c2ea77e829b9a6e91ed
Vinícius <viniciusrx570@gmail.com>
2024-03-08 07:46:01 -0300

    feature: crud de produtos inicializado

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProdutoModule } from './produto/produto.module';
 import { ClienteModule } from './cliente/cliente.module';
+import { VendaModule } from './venda/venda.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, ProdutoModule, ClienteModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, ProdutoModule, ClienteModule, VendaModule],
   controllers: [AppController],
   providers: [AppService],
 })
